57d194d12f6cfd776cce1b78e3f0b6d00a4d3150
carrtealexander <143460245+carrtealexander@users.noreply.github.com>
2023-10-02 23:16:24 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,27 @@
+var submitButton = document.getElementById("submitBtn")
+
+submitButton.addEventListener('click', startGame)
+
+function startGame()
+{
+    console.log('started')
+    
+}
+function setNextQuestion(){
+
+}
+
+
+function selectAnswer(){
+
+
+
+}
+
+const questions = [
+
+    {
+
+        question: 'What is t'
+    }
+]
